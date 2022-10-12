0620eb4880e3eea7afba8953602a637a67f2701d
lttlmw <vvys83@gmail.com>
2022-10-12 14:33:50 +0300

    feat: initial interface

---
+++ include/qinterface/QInterface.h
@@ -0,0 +1,16 @@
+#ifndef HOROSHOEDITOR_QINTERFACE_H
+#define HOROSHOEDITOR_QINTERFACE_H
+
+#include <QWidget>
+#include <QApplication>
+#include <QLabel>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QPushButton>
+
+class QInterface : public QWidget {
+public:
+    QInterface();
+};
+
+#endif //HOROSHOEDITOR_QINTERFACE_H
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <QApplication>
 #include "QImageWidget.h"
+#include "../../include/qinterface/QInterface.h"
 
 
 int main(int argc, char* argv[]) {
@@ -9,23 +10,7 @@ int main(int argc, char* argv[]) {
     std::default_random_engine e;
     QApplication app(argc, argv);
 
-    int height = 900;
-    int width = 1600;
-    auto pixels = (uint8_t*)calloc(height*width*3, 4);
-
-    for (int i = 0; i < height; ++i)
-    {
-        for (int j = 0; j < width; ++j)
-        {
-            for (int z = 0; z < 3; ++z)
-            {
-                pixels[i * width * 3 + j * 3 + z] = e() % 256;
-            }
-        }
-    }
-    auto w = new QImageWidget(pixels, height, width);
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << (end - begin) / pow(10, 9) << " это секунды такие";
-    w->show();
+    auto interface = new QInterface();
+    interface->show();
     return QApplication::exec();
 }
