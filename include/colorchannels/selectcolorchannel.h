5dfddc6f193a819332fcd59b5bf759c8ff045a83
Dmitriy Tikhomirov <uroborosq@ya.ru>
2022-11-05 15:12:33 +0300

    fix: added to index forgotten header

---
+++ include/colorchannels/selectcolorchannel.h
@@ -0,0 +1,9 @@
+//
+// Created by uroborosq on 05.11.22.
+//
+
+#ifndef HOROSHOEDITOR_SELECTCOLORCHANNEL_H
+#define HOROSHOEDITOR_SELECTCOLORCHANNEL_H
+#include <vector>
+std::vector<float>* select_color_channel(const std::vector<float>&, int);
+#endif //HOROSHOEDITOR_SELECTCOLORCHANNEL_H
