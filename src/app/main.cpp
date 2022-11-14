66ffddfe3f576cc8638fdfd5b4d554c218d9abe2
Dmitriy Tikhomirov <uroborosq@ya.ru>
2022-11-14 18:19:00 +0300

    feat: we are in testing

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,16 +5,28 @@
 #include "QMain.h"
 #include "Pixels.h"
 #include "QImageWidget.h"
-
+#include "HSVColorSpace.h"
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
 
-    Pixels* pixels = new Pixels();
+//    auto* pixels = new Pixels();
+//
+//    auto interface = new QMain(pixels);
+//    interface->show();
 
-    auto interface = new QMain(pixels);
-    interface->show();
 
+    Pnm file("1.pnm");
+//    pixels.setColorSpace(ColorSpace::HSV);
+
+    auto converter = HSVColorSpace();
+    file.data = converter.from_rgb(file.data);
+    file.data = converter.to_rgb(file.data);
+    Pixels pixels(file.data, file.height, file.width, file.tag, ColorSpace::RGB, ColorChannel::Все);
+
+
+  auto widget = QImageWidget(&pixels);
+ widget.show();
     return QApplication::exec();
 
 }
