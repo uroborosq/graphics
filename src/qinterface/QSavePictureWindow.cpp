74cb4e5af2f5ee459b9f0b3b4a632d7302d68ba6
lttlmw <vvys83@gmail.com>
2022-11-14 12:22:49 +0300

    feat: added colorspace selection when saving a file

--- src/qinterface/QSavePictureWindow.cpp
+++ src/qinterface/QSavePictureWindow.cpp
@@ -6,18 +6,33 @@ QSavePictureWindow::QSavePictureWindow(Pnm* file) {
 
     this->resize(250, 100);
     auto savePathLine = new QLineEdit();
-    auto label = new QLabel("Введите путь для сохранения файла");
+    auto saveLabel = new QLabel("Введите путь для сохранения файла");
     auto saveButton = new QPushButton("Сохранить");
     saveButton->setAutoDefault(true);
 
+    auto colorspaceLabel = new QLabel("Выберите цветовое пространство изображения");
+    auto colorspaces = new QComboBox();
+    colorspaces->addItem("RGB");
+    colorspaces->addItem("HSL");
+    colorspaces->addItem("HSV");
+    colorspaces->addItem("YCbCr.601");
+    colorspaces->addItem("YCbCr.709");
+    colorspaces->addItem("YCbCg");
+    colorspaces->addItem("CMY");
+
     auto layout = new QVBoxLayout();
-    layout->addWidget(label);
+    layout->addWidget(saveLabel);
     layout->addWidget(savePathLine);
+
+    layout->addWidget(colorspaceLabel);
+    layout->addWidget(colorspaces);
+
     layout->addWidget(saveButton);
     setLayout(layout);
 
     connect(saveButton, &QPushButton::clicked, this, [=]() {
         try {
+            auto colorspaceChoice = colorspaces->currentIndex();
             auto savePicturePath = savePathLine->text().toStdString();
             file->write(savePicturePath);
             this->close();
