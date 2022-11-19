883d23570ffda5640afba4d36f3423eee02fb092
uroborosq <uroborosq@ya.ru>
2022-11-19 13:32:36 +0300

    hotfix: add necessary arguments to constructor's call (а код кто-то запускает перед пушем?)

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,14 +1,13 @@
 #include <QApplication>
 #include "QMain.h"
 #include "Pixels.h"
-
 #include "QImageWidget.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     auto* pixels = new Pixels();
 
-    auto interface = new QMain(pixels);
+    auto interface = new QMain(pixels, new QImageWidget(pixels));
     interface->show();
     return QApplication::exec();
 }
